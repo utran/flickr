5646dbc6cfc0646a4fcf4f69366d80164d12c69b
utran <utran@induscorp.com>
2016-06-28 11:45:36 -0400

    Initial comit

---
+++ app.js
@@ -0,0 +1,22 @@
+//angularjs source file
+'use strict';
+
+//create new module
+var app = angular.module('app', []);
+
+
+//get an existing module
+angular.module('app')
+    .controller('AppCtrl', function(){
+        var vm = this;
+
+        }
+
+        vm.cancel = function () {
+
+        }
+
+        vm.reset = function () {
+           
+        }
+    });
