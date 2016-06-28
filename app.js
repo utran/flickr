ebb04e33209dbdf19d51a39598f9afaea4521747
utran <utran@induscorp.com>
2016-06-28 14:06:49 -0400

    intial commit

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-//angularjs source file
 'use strict';
 
-//create new module
-var app = angular.module('app', []);
+var app = angular.module('app',[]);
+app.controller('AppCtrl', function($http){
+var vm= this;
 
 
-//get an existing module
-angular.module('app')
-    .controller('AppCtrl', function(){
-        var vm = this;
 
-        }
+vm.submit = function () {
+	
+	vm.startSearching = true;
+    var params = {
+    method: 'flickr.photos.search',
+    api_key: '932e0cc959dd923ba91dafb4a7a720ef',
+    tags: vm.search_tag,
+    format: 'json',
+    nojsoncallback: 1
+}
+    
+           $http.get('https://api.flickr.com/services/rest',{params : params})
+            .then(function(res){
+                vm.data = res.data.photos.photo;				
+                })
+				.finally (function () {
+					vm.startSearching = false;
+				});
 
-        vm.cancel = function () {
-
-        }
-
-        vm.reset = function () {
-           
-        }
-    });
+}
+});
